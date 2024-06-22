a597ea4bf0addf995a4928a93aa8ca05aae4f1fd
Yugal Vashishth <yugalvashishth@Yugals-Laptop.local>
2024-06-22 22:23:31 +0530

    added dashboard logic

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
+import { Appbar } from "../components/AppBar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
+
 const Dashboard = () => {
-    return (
-        <>
-            <h1>Dashboard</h1>
-        </>
-    )
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
 }
 
 export default Dashboard;
