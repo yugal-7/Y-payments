d2e0f4597070e1fa060776368324eaa2c180243f
Yugal Vashishth <yugalvashishth@Yugals-Laptop.local>
2024-06-23 19:08:44 +0530

    updated title

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -1,7 +1,7 @@
 export const Appbar = () => {
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
-            PayTM App
+            Y Payments
         </div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4">
